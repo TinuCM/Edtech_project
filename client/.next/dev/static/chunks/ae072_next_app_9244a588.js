745cba9f2935b1b5e2d50c8e982afea931fbf2ce
Tinu C Mathew <tinucherian229@gmail.com>
2025-12-28 13:04:37 +0530

    new

--- client/.next/dev/static/chunks/ae072_next_app_9244a588.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/OneDrive - G10X Technology Private Limited/Desktop/edtech v1/client/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/OneDrive - G10X Technology Private Limited/Desktop/edtech v1/client/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=ae072_next_app_9244a588.js.map
